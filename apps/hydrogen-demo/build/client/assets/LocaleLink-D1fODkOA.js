d704ad8665a43d67cac872b9495db076fb98aeea
Ali Osman <dlsmnosman@gmail.com>
2026-03-07 10:23:34 +0300

    chore: remove hydrogen-demo build artifacts from git

--- apps/hydrogen-demo/build/client/assets/LocaleLink-D1fODkOA.js
+++
@@ -1,1 +0,0 @@
-import{p as o,L as p}from"./chunk-EPOLDU6W-BheWEo7P.js";function e({to:t,locale:s,...n}){const r=s==="en"?"":`/${s}`,i=t.startsWith("/")?`${r}${t}`:t;return o.jsx(p,{to:i,...n})}export{e as L};
--- apps/hydrogen-demo/build/client/assets/messages-Dnh0VNCj.js
+++
@@ -1,1 +0,0 @@
-function a(t,r,e){const o=t==null?void 0:t[r];return typeof o=="string"?o:e}export{a as m};
